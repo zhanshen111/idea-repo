b1034ce0ba457018cab66d1509b90e2e1d039a19
zhan <1955422532@qq.com>
2020-07-04 15:53:24 +0800

    jenkins_test提交

--- jenkins_test/src/main/java/lianxi/hello.java
+++ jenkins_test/src/main/java/lianxi/hello.java
@@ -1,13 +1,34 @@
 package lianxi;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 /**
  * Create By abc on 2020/7/2
  */
 public class hello {
+
     public static void main(String[] args) {
-        System.out.println("hello");
-    }
-    public void test(){
-        System.out.println("hello");
+        Map<Integer,String> map = new HashMap<>();
+        map.put(1, "aaa");
+        map.put(2, "bbb");
+
+        for (Map.Entry<Integer,String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + "=" + entry.getValue());
+        }
+
+        for (Integer key : map.keySet()) {
+            System.out.println(key);
+        }
+        for (String value : map.values()){
+            System.out.println(value);
+        }
+
+        Iterator<Map.Entry<Integer, String>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()){
+            Map.Entry<Integer, String> entry = iterator.next();
+            System.out.println(entry.getKey() + "=" + entry.getValue());
+        }
     }
 }
