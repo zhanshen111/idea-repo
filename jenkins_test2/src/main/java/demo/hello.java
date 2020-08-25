106a94d4dae93f546034eb99aeaf5088fc86b911
zhan <1955422532@qq.com>
2020-08-25 09:30:23 +0800

    jenkins_test2

---
+++ jenkins_test2/src/main/java/demo/hello.java
@@ -0,0 +1,15 @@
+package demo;
+
+/**
+ * Create By abc on 2020/8/25
+ */
+public class hello {
+
+    public static void fun(){
+        System.out.println("hello jenkins");
+    }
+
+    public static void main(String[] args) {
+        fun();
+    }
+}
